b32dc82fc97419208e76d6e6566427a2bb70b626
Diego Chancef <diegoamirch@gmail.com>
2023-07-30 20:12:39 -0500

    modificar_horas

--- js/cargar_htmls.js
+++ js/cargar_htmls.js
@@ -38,25 +38,54 @@ loadHTML("../html/cabecera.html", "Cabecera", function() {
         document.getElementById("overlay").style.display = "block";
         document.getElementById("Formulario_eliminar_cursos_contenido").style.display = "block";
     });
+    // Funcionalidad para aparecer el formulario de Exportar Horarios
     let Importar_horario = document.getElementById("boton_importar_horario");
     Importar_horario.addEventListener("click", function() {
         document.getElementById("overlay").style.display = "block";
         document.getElementById("Formulario_importar_horario_contenido").style.display = "block";
     });
-});
+    
+    loadHTML("../html/tabla.html", "Tabla", function() {
+        let CambiarHorario = document.querySelector("#boton_enviar_2");
+        let Cancelar = document.querySelector("#boton_cancelar_2");
+        let HoraInicioTabla = 6;
+        let HoraFinTabla = 20;
+        let Tabla = document.getElementById("Tabla_horario");
 
-loadHTML("../html/tabla.html", "Tabla", function() {
-    let table = document.getElementById("horario");
-    for (let i = 6; i <= 20; i++) {
-        let row = table.insertRow(-1);
-        let cell = row.insertCell(-1);
-        cell.innerHTML = i + ":00 - " + i + ":59";
-        for (let j = 0; j < 6; j++) {
-            row.insertCell(-1);
+        for (let i = HoraInicioTabla; i <= HoraFinTabla; i++) {
+            let row = Tabla.insertRow(-1);
+            let cell = row.insertCell(-1);
+            cell.innerHTML = i + ":00 - " + i + ":59";
+            for (let j = 0; j < 6; j++) {
+                row.insertCell(-1);
+            }
         }
-    }
+        //Boton Submit
+        CambiarHorario.addEventListener("click", function() {
+            HoraInicioTabla = parseInt(document.getElementById("hora_inicio_tabla").value);
+            HoraFinTabla = parseInt(document.getElementById("hora_fin_tabla").value);
+            let Tabla = document.getElementById("Tabla_horario");
+            while (Tabla.rows.length > 1) {
+                Tabla.deleteRow(1);
+            }
+            for (let i = HoraInicioTabla; i <= HoraFinTabla; i++) {
+                let row = Tabla.insertRow(-1);
+                let cell = row.insertCell(-1);
+                cell.innerHTML = i + ":00 - " + i + ":59";
+                for (let j = 0; j < 6; j++) {
+                    row.insertCell(-1);
+                }
+            }
+            document.getElementById("Formulario_modificar_horas_contenido").style.display = "none";
+            document.getElementById("overlay").style.display = "none";
+        });
+        // Boton Cancelar
+        Cancelar.addEventListener("click", function() {
+            document.getElementById("Formulario_modificar_horas_contenido").style.display = "none";
+            document.getElementById("overlay").style.display = "none";
+        });
+    });
 });
-
 
 // Importacion de los formularios
 loadHTML("../html/formularios/formulario_modificar_dias.html", "Formulario_modificar_dias");  
